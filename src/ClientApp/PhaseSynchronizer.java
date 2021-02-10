a65102c1b8e3e0df88c41c69b85a2aae9d407a68
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-10 16:05:08 +0100

    dodano podstawową sychronizację między oknami

---
+++ src/ClientApp/PhaseSynchronizer.java
@@ -0,0 +1,12 @@
+package ClientApp;
+
+public class UsageSynchronizer {
+
+    // this class will sychronize changes between two accessible states of an app
+    // which are Logged and NotLogged
+    // it consists of thread that listes to changes in loginWindow and in mainWindow
+
+
+
+
+}
