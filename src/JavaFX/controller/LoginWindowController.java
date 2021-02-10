839c1cf267df2e41e2a745be381a05f34fb503d9
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-10 15:38:50 +0100

    dodano detale w oknie logowania

--- src/JavaFX/controller/LoginWindowController.java
+++ src/JavaFX/controller/LoginWindowController.java
@@ -1,14 +1,83 @@
 package JavaFX.controller;
 
-import com.sun.javafx.geom.Rectangle;
-import javafx.event.EventHandler;
+//import com.sun.javafx.geom.Rectangle;
+import Colors.LoginButtonColors;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class LoginWindowController {
 
+    private Color defaultLogin;
+    private Color defaultRegistration;
+
+    private String password = "jebacpis";
+    private String login = "Andrzej";
+
+    private LoginButtonColors probe = new LoginButtonColors();
+
     /* Here controlling all information flow and configuration of logging window */
     @FXML
-    javafx.scene.shape.Rectangle loginButton;
+    Button loginButton;
+
+    @FXML
+    Button registerButton;
+
+    @FXML
+    Rectangle loginRectangle;
+
+    @FXML
+    Rectangle registrationRectangle;
+
+    @FXML
+    PasswordField passwordTextField;
+
+    @FXML
+    TextField loginTextField;
+
+    void initialize() {
+    }
+
+    @FXML
+    void loginButtonClicked() {
+        /* Parse data and check if it is correct */
+        if ( password.equals(passwordTextField.getText()) && login.equals(loginTextField.getText())) {
+            System.out.println("Udało się zalogować do systemu!");
+            
+        }
+        else {
+            System.out.println("Odmowa dostępu!");
+        }
+    }
+
+    @FXML
+    void loginButtonEntered() {
+        loginRectangle.setFill(probe.loginActive);
+    }
+
+    @FXML
+    void loginButtonExited() {
+        loginRectangle.setFill(probe.loginInactive);
+    }
+
+    @FXML
+    void registerButtonEntered() {
+        registrationRectangle.setFill(probe.registerActive);
+
+    }
+
+    @FXML
+    void registerButtonExited() {
+        registrationRectangle.setFill(probe.registerInactive);
+    }
+
+    @FXML
+    void registerButtonClicked() {
+        /* Direct user to registration form */
+    }
 
     // TODO -- connetct my two windows with some logic
 
@@ -16,7 +85,4 @@ public class LoginWindowController {
 
     }
 
-    void initialize() {
-
-    }
 }
