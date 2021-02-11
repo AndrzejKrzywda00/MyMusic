fe631c3a3e8631f85d8fb13603b0be878679619a
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-11 15:47:22 +0100

    poprawiono niepotrzebne wywołania funkcji set na obiekcie phaseSynch

--- src/ClientApp/MyMusicGUI.java
+++ src/ClientApp/MyMusicGUI.java
@@ -83,9 +83,7 @@ public class MyMusicGUI extends Application {
 
         // one window can be visible at a time
         // TODO - make this an extensible element -- possibly with some listeners
-
-        phaseSynch.setPrmiaryStage(primaryStage);
-        phaseSynch.setLoggingStage(loggingStage);
+        
 
     }
 }
