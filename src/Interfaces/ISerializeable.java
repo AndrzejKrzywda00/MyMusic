51b4640e069f6d02385d2241ccb5b60b2450b7bb
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-11 19:21:53 +0100

    dodano interfejs do serializacji

---
+++ src/Interfaces/ISerializeable.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+public interface ISerializeable {
+
+    /* as name suggests this interface is to force serialization */
+
+    public String serialize();  // not entirely sure if this sholud have String or StringBuilder
+
+}
