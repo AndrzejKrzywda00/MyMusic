5a0eb8eb398e8315182de73e66e8c2d301ef749c
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-11 19:07:53 +0100

    dodano szczegóły do menu nowego utworu

---
+++ src/JavaFX/controller/addTrackMenuController.java
@@ -0,0 +1,21 @@
+package JavaFX.controller;
+
+public class addTrackMenuController {
+
+    // This clas is the controller of the menu that adds a new track
+
+    // WARNING - you can have cosillion here of two controllers when adding this to mainWindow.fxml
+
+    // constructor
+    public addTrackMenuController() {
+
+    }
+
+    // initalization
+    public void initialize() {
+
+    }
+
+    
+
+}
