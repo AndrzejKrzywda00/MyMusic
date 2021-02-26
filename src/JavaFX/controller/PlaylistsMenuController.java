f762bda923a6ace3332c48a65d241adb46474588
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-02-26 21:00:00 +0100

    dodano elementy responsywne w oknie dodawania playlist

---
+++ src/JavaFX/controller/PlaylistsMenuController.java
@@ -0,0 +1,31 @@
+package JavaFX.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PlaylistsMenuController {
+
+    @FXML
+    TextField titleTextField;
+
+    @FXML
+    TextField descriptionTextField;
+
+    @FXML
+    Button addThumbnailButton;
+
+    @FXML
+    Button cancelButton;
+
+    @FXML
+    Button confirmButton;
+
+
+    public void initialize() {
+
+    }
+
+    
+
+}
