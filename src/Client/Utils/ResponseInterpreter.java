79266ccfa7eb823697fafa3ffee7055749ae6a02
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-03-07 11:18:45 +0100

    zbduowano szkielet interpretacji odpowiedzi http

--- src/Client/Utils/ResponseInterpreter.java
+++ src/Client/Utils/ResponseInterpreter.java
@@ -2,6 +2,7 @@ package Client.Utils;
 
 import Client.Utils.enums.Headers;
 import Client.Utils.enums.Status;
+import com.sun.javafx.scene.shape.MeshHelper;
 import enums.MessageType;
 
 import java.util.Map;
@@ -25,10 +26,11 @@ public class ResponseInterpreter {
 
         if (response.getStatusMessage() == Status.OK_200.message || response.getStatusMessage() == Status.Created_201.message) {
             Map<String, String> headers = response.getHeaders();
+            String serverName = "MyMusic Server";
             if (headers.getOrDefault(Headers.ContentType.name, null) != null
                     && headers.getOrDefault(Headers.ContentLength.name, null) != null
                     && headers.getOrDefault(Headers.Date.name, null) != null
-                    && headers.getOrDefault(Headers.ServerName.name, null) != null
+                    && headers.getOrDefault(Headers.ServerName.name, serverName) != null
                     && headers.getOrDefault(Headers.ApplicationID.name, null) != null) {
                 // this means all required headers are present and have their not-null payload
                 if (response.getBody() != null) {
@@ -43,8 +45,28 @@ public class ResponseInterpreter {
 
         if (validateResponse(response)) {   // response fulfills the requirements and should be interpreted
             Map<String, String> headers = response.getHeaders();
-            if(headers.get(Headers.ContentType.name) == null) {
 
+            if(headers.get(Headers.ContentType.name) == MessageType.LoginMessage.name) {
+                // this means we have logging information inside
+            }
+
+            if(headers.get(Headers.ContentType.name) == MessageType.NewPlaylistMessage.name) {
+                // new playlist BACK information
+                // we know it has been accepted
+                // body has at least one line of data inside
+                // depends how will i organize this
+            }
+
+            if(headers.get(Headers.ContentType.name) == MessageType.NewTrackMessage.name) {
+                // new track is accepted
+                // data from it goes back to be added to main list
+            }
+
+            if(headers.get(Headers.ContentType.name) == MessageType.TracksDataMessage.name) {
+                // this one is big
+                // here i get many lines of tracks
+                // comes from server on normal download process
+                // this should be maximum of [DataDownloadConfiguration.DISPLAY_CAPACITY] lines
             }
         }
 
--- src/Client/configuration/DataDownloadConfiguration.java
+++ src/Client/configuration/DataDownloadConfiguration.java
@@ -2,7 +2,7 @@ package Client.configuration;
 
 public class DataDownloadConfiguration {
 
-    public static final int DISPLAY_CAPASITY = 200;     // this is maximum of how many tracks will be downloaded
+    public static final int DISPLAY_CAPACITY = 200;     // this is maximum of how many tracks will be downloaded
 
     // data will be taken on demand from server
     // request will be built
